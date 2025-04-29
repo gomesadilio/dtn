e9ed29252ee62a8b9d2a3715fe248524738c8dcc
gomesadilio <70861135+gomesadilio@users.noreply.github.com>
2025-04-28 22:12:35 -0300

    Update main.py

--- main.py
+++ main.py
@@ -9,7 +9,6 @@ def to_str(some_date):
 def to_num(some_date):
     return (some_date - dt(1899,12,30)).days
 
-
 day, month, year = (0,0,0)
 
 if len(sys.argv) > 1:
@@ -17,26 +16,31 @@ if len(sys.argv) > 1:
 
     try:
         day, month, year = [int(''.join(param[(k-1)*2: k*2])) for k in range(1, len(param)//2 +1)]
-    except ValueError:
-        print('Erro ao converter o valor recebido, será usado o dia atual!')
+    except Exception:
+        pass
 
 day = day if day else selected_date.day
 month = month if month else selected_date.month
 year = 2000 + year if year else selected_date.year
 
-selected_date = selected_date.replace(day= day, month = month, year = year)
+try:
+    selected_date = selected_date.replace(day= day, month = month, year = year)
+except ValueError:
+    print('Erro ao converter o valor recebido, será usado o dia atual!')
+
+next_month = 1 if selected_date.month == 12 else selected_date.month
+next_year = selected_date.year +1 if selected_date.month == 12 else selected_date.year
 
 dict_dates = {
     'Data base' : selected_date,
     'Dia anterior' : selected_date - td(days=1),
     'Início do mês' : selected_date.replace(day=1),    
-    'Final do mês' : selected_date.replace(day=1, month = selected_date.month + 1) - td(days=1),
+    'Final do mês' : selected_date.replace(day=1, month = next_month, year=next_year) - td(days=1),
     'Início do ano' : selected_date.replace(day=1,month=1),
     'Final do ano' : selected_date.replace(day=31,month=12)
 }           
 
-if day or month or year:
-    print(f"{'*'*50}\ndata solicitada: dia = {day}, mês = {month}, ano = {year}")
+print(f"{'*'*50}\ndata solicitada: dia = {selected_date.day}, mês = {selected_date.month}, ano = {selected_date.year}")
 
 cnt = 0
 for k, v in dict_dates.items():
